b999a7f6e3b2d729d8582756f410d24c2d0eaeeb
Tommy Urrea <tommy.urrea@gmail.com>
2016-02-29 15:03:52 -0800

    minor formatting updates; remove unused regex

--- articles.js
+++ articles.js
@@ -1,15 +1,15 @@
 (function() {
   'use strict';
-  
-  angular
-  	.module('articles', ['ngResource'])
-  	.value('ARTICLES_API_URL', 'http://www.stellarbiotechnologies.com/media/press-releases/json')
+
+  angular.module('articles', ['ngResource'])
+  	.constant('ARTICLES_API_URL', 'http://www.stellarbiotechnologies.com/media/press-releases/json')
   	.factory('Articles', Articles);
 
   function Articles($resource, ARTICLES_API_URL) {
     var articlesResource = $resource(ARTICLES_API_URL);
     return articlesResource;
   }
+
   Articles.$inject = ['$resource', 'ARTICLES_API_URL'];
-  
+
 })();
--- script.js
+++ script.js
@@ -2,20 +2,19 @@
 
 (function(){
   'use strict';
-  
-  angular
-    .module('demoApp', ['articles', 'infinite-scroll'])
+
+  angular.module('demoApp', ['articles', 'infinite-scroll'])
     .controller('ArticlesController', ArticlesController);
 
     function ArticlesController(Articles) {
       var vm = this;
+
       vm.articles = [];
       vm.params = {
         offset: 0,
         limit: 20
       };
       vm.getArticles = getArticles;
-      vm.onlyPositiveNumbers = /^(\d)+$/;
       vm.loading = false;
 
       function getArticles() {
@@ -27,13 +26,14 @@
 
         Articles.get(vm.params).$promise.then(function(data) {
           vm.articles = vm.articles.concat(data.news);
-          
+
           //increment offset and limit
           vm.params.offset += vm.params.limit;
           vm.loading = false;
         });
       }
     }
+    
     ArticlesController.$inject = ['Articles']
 
 })();
